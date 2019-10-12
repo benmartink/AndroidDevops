68fa08e24c424e684c864b6d3c5b6015c2b4cf50
Ben Martink <ben.martink@gmail.com>
2019-10-12 02:57:29 -0500

    Revert "Cambio en feature 1"
    This reverts commit 41dfd656323a85999ce79d0dccb139e7ed09f70a.

--- MyApplication2/app/src/main/java/com/devops/demo/MainActivity.java
+++ MyApplication2/app/src/main/java/com/devops/demo/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-//Cambio en feature 1
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
